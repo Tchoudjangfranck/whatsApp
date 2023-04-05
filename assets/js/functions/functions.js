7cf88256eb494056784ceab740104d7da17d9a15
Tchoudjang Ndokou Michel Franck <tchoudjangfranck@gmailcom>
2023-04-05 19:30:17 +0100

    whastApp - prjet complet

--- assets/js/functions/functions.js
+++ assets/js/functions/functions.js
@@ -11,8 +11,7 @@ let getCorrespondents = async () =>
     //    console.log(dataCorrespondents);
     return dataCorrespondents;
 }
-
-export let displayCorrespondents = async () =>
+let displayCorrespondents = async () =>
 {
     let discussion = document.getElementById("discussion");
     discussion.innerHTML=""
@@ -42,6 +41,7 @@ export let displayCorrespondents = async () =>
         let image  = document.createElement("img");
         image.src = `${correspondent.image}`;
         image.width = "35";
+        image.height = "35";
         correspondentImage.appendChild(image);
         discussionItem.appendChild(correspondentImage);
         
@@ -64,13 +64,55 @@ export let displayCorrespondents = async () =>
         discussionItem.appendChild(discussioCurrentTime);
         discussion.appendChild(discussionItem)
         return discussionItem;
-
     });
+}
+
+displayCorrespondents();
+
+let displayContacts = async () =>
+{
+    let contactsContainer = document.getElementById("contacts");
+    contactsContainer.innerHTML=""
+    let contacts = await getCorrespondents();
+    // console.log(contacts);
+    contacts.forEach(contact => 
+    {
+        let contactItem = document.createElement("div");
+        contactItem.classList.add("contact-item", "flex", "aic", "gap-10");
+        contactItem.style.userSelect = "none";           
+        let contactImage  = document.createElement("div");
+        contactImage.classList.add("contact-image");
+        let image  = document.createElement("img");
+        image.src = `${contact.image}`;
+        image.width = "35";
+        image.height = "35";
+        contactImage.appendChild(image);
+        contactItem.appendChild(contactImage);
+        
+        let contactContent  = document.createElement("div");
+        contactContent.classList.add("contact-content", "flex-1");
+        let contactPseudo  = document.createElement("div");
+        contactPseudo.classList.add("contact-pseudo");
+        contactPseudo.innerText = `${contact.name}`;
+        contactContent.appendChild(contactPseudo);
+
+        let contactStartingMessage  = document.createElement("div");
+        contactStartingMessage.classList.add("contact-starting-message", "flex-1");
+        contactStartingMessage.innerText = `${contact.slogan}`;
+        
+        contactContent.appendChild(contactStartingMessage);
+        contactItem.appendChild(contactContent);
+
+        contactsContainer.appendChild(contactItem);
+        
+    });
+    // console.log(contactsContainer);
+        return contactsContainer;
 
 
 }
 
-displayCorrespondents();
+// displayContacts();
 
 let getSettingOptions = async () =>
 {
@@ -80,7 +122,112 @@ let getSettingOptions = async () =>
     {
         return;
     }
-     let dataSettingOptions = await data.json();
+    let dataSettingOptions = await data.json();
     //    console.log(dataSettingOptions);
     return dataSettingOptions;
 }
+
+let getProfilOptions = async () =>
+{
+    let request = "/assets/data/profilOptions.json";
+    let data = await fetch(request);
+    if(!data.ok)
+    {
+        return;
+    }
+    let dataProfilOptions = await data.json();
+    //    console.log(dataProfilOptions);
+    return dataProfilOptions;
+} 
+
+let displayProfilOptions = async () =>
+{
+    let profilOptionsBox = document.createElement("ul");
+    profilOptionsBox.classList.add("profil-options");
+    
+    let profilOptions = await getProfilOptions();
+    profilOptions.forEach(profilOption => 
+    {
+        let profilOptionItem = document.createElement("li");
+        profilOptionItem.classList.add("profil-option", "flex", "gap-10", "aic");
+        
+        let profilOptionIcon = document.createElement("i");
+        profilOptionIcon.className = `profil-option-icon ${profilOption.profilOptionIcon}`;
+        
+        let profilOptionTitle = document.createElement("div");
+        profilOptionTitle.classList.add("profil-option-title", "flex-1");
+        profilOptionTitle.innerHTML = `${profilOption.profilOptionTitle}`;
+        
+        
+        let profilOptionModified = document.createElement("i");
+        profilOptionIcon.className = `profil-option-modified fas fa-pen`;
+        
+        profilOptionItem.appendChild(profilOptionIcon);
+        profilOptionItem.appendChild(profilOptionTitle);
+        profilOptionItem.appendChild(profilOptionModified);
+
+        profilOptionsBox.appendChild(profilOptionItem);
+    });
+
+    return profilOptionsBox;
+}
+
+let getStickersOptions = async () =>
+{
+    let request = "/assets/data/stickersOptions.json";
+    let data = await fetch(request);
+    if(!data.ok)
+    {
+        return;
+    }
+    let dataStickersOptions = await data.json();
+    //    console.log(dataStickersOptions);
+    return dataStickersOptions;
+}
+
+let displayStickersOptions = async () =>
+{
+    let stickersOptionContainer = document.createElement("ul");
+    stickersOptionContainer.classList.add("stickers-options");
+    
+    
+    let stickersOptions = await getStickersOptions();
+    stickersOptions.forEach(stickersOption =>
+    {
+        // console.log(stickersOption);
+        let stickersOptionItem = document.createElement("li");
+        stickersOptionItem.classList.add("stickers-option", "flex", "aic", "gap-20");
+
+        let stickersOptionIconContainer = document.createElement("div");
+        stickersOptionIconContainer.classList.add("stickers-option-icon");
+        stickersOptionIconContainer.style.backgroundColor = `${stickersOption.stickersOptionIconBackgrongColor}`
+
+        let stickersOptionIcon = document.createElement("i");
+        stickersOptionIcon.classList.add("bi", "bi-tag");
+        
+        let optionDescription = document.createElement("div");
+        optionDescription.classList.add("option-description");
+
+        let optionName = document.createElement("div");
+        optionName.classList.add("option-name");
+        optionName.innerHTML = `${stickersOption.stickersOptionName}`;
+        
+        let optionDetails = document.createElement("div");
+        optionDetails.classList.add("option-details");
+        optionDetails.innerHTML = `${stickersOption.stickersOptionDetails}`;
+
+        stickersOptionIconContainer.appendChild(stickersOptionIcon);
+
+        optionDescription.appendChild(optionName);
+        optionDescription.appendChild(optionDetails);
+
+        stickersOptionItem.appendChild(stickersOptionIconContainer);
+        stickersOptionItem.appendChild(optionDescription);
+            
+        stickersOptionContainer.appendChild(stickersOptionItem);    
+    })
+    // console.log(stickersOptionContainer);
+    return stickersOptionContainer;
+} 
+
+displayStickersOptions();
--- assets/js/functions/listeners.js
+++ assets/js/functions/listeners.js
@@ -1,58 +1,128 @@
-import { displayCorrespondents } from "./functions.js";
-
-let profilTop = document.getElementById("profil-top").innerHTML;
-let profilBody = document.getElementById("discussion").innerHTML;
-let profilContentTop = profilTop;
-// displayCorrespondents();
-let ss = async () =>
-{
-    let dd =displayCorrespondents();
-    console.log(dd);
-}
-
-ss()
-
 let listenersFunction =
 {
     mainhome: document.getElementById("main-home"),
     maincontent: document.getElementById("main-content"),
-    profil: document.getElementById("profil"),
-    profilTop: document.getElementById("profil-top"),
-    profilBody: document.getElementById("profil-body"),
+    aside: document.getElementById("aside"),
+    asideHome: document.getElementById("aside-home"),
+    asideSetting: document.getElementById("aside-setting"),
+    asideSettingBody: document.getElementById("aside-setting-body"),
+    asideProfil: document.getElementById("aside-profil"),
+    asideProfilBody: document.getElementById("aside-profil-body"),
+    asideStickers: document.getElementById("aside-stickers"),
+    asideContact: document.getElementById("aside-discussion"),
+    asideUserStickers: document.getElementById("user-stickers"),
+    asideDiscussion: document.getElementById("aside-discussion"),
     mainbox: document.getElementById("main-box"),
     moreOption: document.getElementById("more-options"),
     
     displayMessages: function()
     {
-        // console.log(this);
         listenersFunction.mainhome.style.display ="none";   
         listenersFunction.maincontent.style.display ="block";   
-        // listenersFunction.maincontent.classList.remove("none");
         listenersFunction.mainbox.style.borderBottom = "none";
         let bodyWidth = document.body.clientWidth;
+
         // console.log(bodyWidth);
         if (bodyWidth <= 970)
         {
-            listenersFunction.profil.style.display ="none";
+            listenersFunction.aside.style.display ="none";
             listenersFunction.mainbox.style.display = "block";
         }
-        
-        // console.log(displayCorrespondents());
     },
 
-    displayProfil: function()
+    displayAside: function()
     {
-        listenersFunction.profil.style.display ="block";
+        listenersFunction.aside.style.display ="block";
         listenersFunction.mainbox.style.display = "none";
-        
     },
 
-    displayProfilDescription: function()
+    displayAsideProfil: async function()
     {
-        listenersFunction.profilTop.    innerHTML = "";
-        listenersFunction.profilBody.innerHTML = "";
-    },
+        listenersFunction.asideHome.style.display = "none";
+        listenersFunction.asideProfil.style.display = "block";
+        listenersFunction.asideProfilBody.innerHTML = "";
 
+        let asideProfilImageContainer = document.createElement("div");
+        asideProfilImageContainer.classList.add("profil-image-container", "relative");
+        
+        let asideProfilImage = document.createElement("div");
+        asideProfilImage.classList.add("profil-image", "absolute");
+        
+        let ownerProfiImage = document.createElement("div");
+        ownerProfiImage.classList.add("owner-image",  "relative");
+        
+        let image = document.createElement("img");
+        image.src = "/assets/images/logo/franck.jpg";
+        image.id = "owner-image";
+        image.addEventListener("mouseenter", listenersFunction.displayBoxShadowImage);
+        image.addEventListener("mouseout", listenersFunction.hideBoxShadowImage);
+        
+        let boxShadowImage = document.createElement("div");
+        boxShadowImage.classList.add("box-shadow-image", "flex", "column", "gap-10", "jcc", "aic", "text-center", "absolute");
+        boxShadowImage.id="box-shadow-image";
+
+        let profilIcon = document.createElement("i");
+        profilIcon.classList.add("fas", "fa-image");
+
+        let profilName = document.createElement("div");
+        profilName.classList.add("profil-name");
+        profilName.innerHTML = "changer la photo de profil";
+        
+        let profilIconImage = document.createElement("i");
+        profilIconImage.classList.add("fas", "fa-pen", "absolute");
+
+        boxShadowImage.appendChild(profilIcon);
+        boxShadowImage.appendChild(profilName);
+
+        ownerProfiImage.appendChild(image);
+        ownerProfiImage.appendChild(boxShadowImage);
+
+        asideProfilImage.appendChild(ownerProfiImage);
+        asideProfilImageContainer.appendChild(asideProfilImage);
+        asideProfilImage.appendChild(profilIconImage);
+        listenersFunction.asideProfilBody.appendChild(asideProfilImageContainer);
+
+        let profilDetailsContainer = document.createElement("div");
+        profilDetailsContainer.classList.add("profil-details-container");
+        
+        let profilOwnerInfos = document.createElement("div");
+        profilOwnerInfos.classList.add("owner-infos", "flex", "gap-10");
+
+        let ownerInfosIcon = document.createElement("i");
+        ownerInfosIcon.classList.add("fas", "fa-portrait", "relative");
+
+        let ownerDetails = document.createElement("div");
+        ownerDetails.classList.add("owner-details");
+        
+        let ownerName = document.createElement("div");
+        ownerName.classList.add("owner-name");
+        ownerName.innerHTML = "Tchoudjang franck";
+        
+        let ownerVisibility = document.createElement("div");
+        ownerVisibility.classList.add("owner-visibility");
+        ownerVisibility.innerHTML = `Ce nom séra visible par vos client(e)s WhatsApp 
+                                    et pourra etre modifié à partir de l'application 
+                                    sur voter appareil mobile`;
+
+
+        profilOwnerInfos.appendChild(ownerInfosIcon);
+        profilOwnerInfos.appendChild(ownerDetails);
+
+        ownerDetails.appendChild(ownerName);
+        ownerDetails.appendChild(ownerVisibility);
+
+        profilDetailsContainer.appendChild(profilOwnerInfos);
+
+        let profilOptionsBox = await displayProfilOptions();
+        // console.log(profilOptionsBox);
+        listenersFunction.asideProfilBody.appendChild(profilDetailsContainer);
+        listenersFunction.asideProfilBody.appendChild(profilOptionsBox);
+
+        // console.log(asideProfilBody);
+
+        listenersFunction.asideProfil.appendChild(listenersFunction.asideProfilBody);
+    },
+    
     displayMoreOptionsBloc: function()
     {
         listenersFunction.moreOption.style.display = "block";
@@ -65,37 +135,17 @@ let listenersFunction =
 
     displaySettingBloc: async function()
     {
-        listenersFunction.profilTop.innerHTML = "";
-        
-        let profilDescriptionTop = document.createElement("div");
-        profilDescriptionTop.classList.add("profil-description-top", "flex", "aie", "gap-20");
-        
-        let arrowLeft = document.createElement("i");
-        arrowLeft.addEventListener("click", () =>
-        {    
-            // listenersFunction.profil.innerHTML = profilContent;            
-            // console.log(profilContent);
-        })
-        arrowLeft.classList.add("fas", "fa-arrow-left");
-        profilDescriptionTop.appendChild(arrowLeft);
+        listenersFunction.asideHome.style.display = "none";
+        listenersFunction.asideSetting.style.display = "block";
 
-        let description = document.createElement("div");
-        description.innerHTML = "Paramètres";
-        description.classList.add("description");
-        profilDescriptionTop.appendChild(description);
-        listenersFunction.profilTop.appendChild(profilDescriptionTop);
-        // console.log(profilDescriptionTop);
-
-        listenersFunction.profilBody.innerHTML = "";
-
-        let profilDescriptionBody = document.createElement("div");
-        profilDescriptionBody.classList.add("profil-description-body");
+        let settingDescriptionBody = document.createElement("div");
+        settingDescriptionBody.classList.add("setting-description-body");
         
-        let userProfil = document.createElement("div");
-        userProfil.classList.add("user-profil");
+        let userSetting = document.createElement("div");
+        userSetting.classList.add("user-setting");
         
-        let userProfilDetails = document.createElement("div");
-        userProfilDetails.classList.add("user-profil-details", "flex", "gap-20", "aic");
+        let userSettingDetails = document.createElement("div");
+        userSettingDetails.classList.add("user-setting-details", "flex", "gap-20", "aic");
 
         let profilImage = document.createElement("div");
         profilImage.classList.add("profil-image");
@@ -103,9 +153,10 @@ let listenersFunction =
         let image = document.createElement("img");
         image.src = "/assets/images/logo/franck.jpg";
         image.width = "40";
+        image.height = "40";
         profilImage.appendChild(image); 
 
-        userProfilDetails.appendChild(profilImage);
+        userSettingDetails.appendChild(profilImage);
 
         let userPseudo = document.createElement("div");
         userPseudo.classList.add("user-pseudo");
@@ -120,9 +171,9 @@ let listenersFunction =
         userSlogan.innerHTML = "C'est le travail qui paie !";
         userPseudo.appendChild(userSlogan);
 
-        userProfilDetails.appendChild(userPseudo);
+        userSettingDetails.appendChild(userPseudo);
 
-        userProfil.appendChild(userProfilDetails);
+        userSetting.appendChild(userSettingDetails);
         
         let userOptions = document.createElement("ul");
         userOptions.classList.add("user-options");
@@ -147,29 +198,84 @@ let listenersFunction =
         });
         // console.log(settingOptions);        
 
-        userProfil.appendChild(userOptions);
+        userSetting.appendChild(userOptions);
 
-        profilDescriptionBody.appendChild(userProfil);
-        listenersFunction.profilBody.appendChild(profilDescriptionBody);
-        // console.log(profilDescriptionBody);
+        settingDescriptionBody.appendChild(userSetting);
+        listenersFunction.asideSettingBody.innerHTML = "";
+        listenersFunction.asideSettingBody.appendChild(settingDescriptionBody);
+        // console.log(settingDescriptionBody);
     },
+    displayAsideStickers: async function()
+    {   
+        listenersFunction.asideHome.style.display = "none";
+        listenersFunction.asideStickers.style.display = "block";
+        listenersFunction.asideUserStickers.innerHTML = "";
+
+        let stickersOptions = await displayStickersOptions();
+        listenersFunction.asideUserStickers.appendChild(stickersOptions);
+    },
+    displayAsidecontact: async function()
+    {   
+        listenersFunction.asideHome.style.display = "none";
+        listenersFunction.asideContact.style.display = "block";
+        // listenersFunction.asideContact.innerHTML = "";
+
+        let contactOptions = await displayContacts();
+        listenersFunction.asideContact.appendChild(contactOptions);
+    },
+    displayAsideBlocs: function ()
+    {
+        listenersFunction.asideHome ? listenersFunction.asideHome.style.display = "block" : null;            
+        listenersFunction.asideSetting ? listenersFunction.asideSetting.style.display = "none" : null;
+        listenersFunction.asideProfil ? listenersFunction.asideProfil.style.display = "none" : null;
+        listenersFunction.asideStickers ? listenersFunction.asideStickers.style.display = "none" : null;
+        listenersFunction.asideDiscussion ? listenersFunction.asideDiscussion.style.display = "none" : null;
+    },
+    displayBoxShadowImage: function()
+    {
+        let boxShadowImage = document.getElementById("box-shadow-image");
+        boxShadowImage.style.display = "flex";
+    },
+    hideBoxShadowImage: function()
+    {
+        let boxShadowImage = document.getElementById("box-shadow-image");
+        boxShadowImage.style.display = "none";
+    }
 }
 
 let setUpListeners = () =>
 {
     let discussionItems = document.getElementsByClassName("discussion-item");
 
-    for (let index = 0; index < discussionItems.length; index++) {
+    for (let index = 0; index < discussionItems.length; index++) 
+    {
         const discussionItem = discussionItems[index];
         
         discussionItem.addEventListener("click", listenersFunction.displayMessages);    
     }
 
     let burgerIcon = document.getElementById("burger-icon");
-    burgerIcon.addEventListener("click", listenersFunction.displayProfil);
-
+    burgerIcon.addEventListener("click", listenersFunction.displayAside);
     let profilImage = document.getElementById("profil-image");
-    profilImage.addEventListener("click", listenersFunction.displayProfilDescription);
+    profilImage.addEventListener("click", listenersFunction.displayAsideProfil);
+    
+    let asideSettingDescriptionIcon = document.getElementById("aside-setting-icon");
+    asideSettingDescriptionIcon.addEventListener("click", listenersFunction.displayAsideBlocs);
+    
+    let asideProfilIcon = document.getElementById("aside-profil-icon");
+    asideProfilIcon.addEventListener("click", listenersFunction.displayAsideBlocs);
+    
+    let asideStickersIcon = document.getElementById("aside-stickers-icon");
+    asideStickersIcon.addEventListener("click", listenersFunction.displayAsideBlocs);
+    
+    let showStickers =document.getElementById("show-stickers");
+    showStickers.addEventListener("click", listenersFunction.displayAsideStickers);
+    
+    let showContact = document.getElementById("show-contact");
+    showContact.addEventListener("click", listenersFunction.displayAsidecontact);
+    
+    let asideContactIcon = document.getElementById("aside-discussion-icon");
+    asideContactIcon.addEventListener("click", listenersFunction.displayAsideBlocs);
 
     let ellipsisIcon = document.getElementById("ellipsis-icon");
     ellipsisIcon.addEventListener("click", listenersFunction.displayMoreOptionsBloc);
@@ -178,4 +284,4 @@ let setUpListeners = () =>
 
     let settingIcon = document.getElementById("setting");
     settingIcon.addEventListener("click", listenersFunction.displaySettingBloc);
-}
+ }
